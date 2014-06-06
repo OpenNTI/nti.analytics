f5535c7d9f1e6004a2b7215d9a033fa1ff9e2ee9
Josh Zuech <josh.zuech@nextthought.com>
2014-06-06 00:36:14 +0000

    Add import and zcml test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@40657 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/analytics/database/tests/test_import.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+.. $Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import unittest
+
+from tempfile import mkstemp
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.dataserver.users import User
+
+from nti.analytics.admin_views import init_db
+from nti.analytics.admin_views import init
+
+import nti.testing.base
+
+from ..database import AnalyticsDB
+from ..metadata import Users
+
+from hamcrest import assert_that
+from hamcrest import has_length
+from hamcrest import none
+from hamcrest import not_none
+from hamcrest import has_property
+
+from nti.dataserver.tests.mock_dataserver import SharedConfiguringTestLayer, WithMockDSTrans
+
+ZCML_STRING = """
+<configure	xmlns="http://namespaces.zope.org/zope"
+			xmlns:i18n="http://namespaces.zope.org/i18n"
+			xmlns:zcml="http://namespaces.zope.org/zcml"
+			xmlns:adb="http://nextthought.com/analytics/database">
+
+	<include package="zope.component" file="meta.zcml" />
+	<include package="zope.security" file="meta.zcml" />
+	<include package="zope.component" />
+	
+	<utility component="nti.analytics.entities" provides="nti.analytics.interfaces.IObjectProcessor" name="entities"/>
+	
+</configure>
+"""
+# 
+# class TestImportFromDS(ApplicationLayerTest):
+# 	@WithSharedApplicationMockDS(users=True,testapp=True,default_authenticate=True)
+# 	def test_import(self):
+# 		
+# 		#sj = 'sjohnson@nextthought.com'
+# 		
+# 		# ComponentLookupError: (<InterfaceClass zope.intid.interfaces.IIntIds>, '')?
+# 		# Mock DS does not have this component?
+# 		#init_db( self.db, usernames=[sj] )
+# 		
+# 		uid = 01
+# 		entity = user1 = User.create_user( self.ds, username='foo@bar' )
+# 		init( uid, self.db, entity )
+		
+# class TestImportFromDS(unittest.TestCase):
+# 
+# 	layer = SharedConfiguringTestLayer
+# 	
+# 	def setUp(self):
+#  		_, self.filename = mkstemp()
+# 		uri = 'sqlite:///%s' % self.filename
+# 		self.db = AnalyticsDB( dburi=uri )	
+# 
+# 	@WithMockDSTrans
+# 	def test_import(self):
+# 		session = self.db.get_session()
+# 		results = session.query(Users).all()
+# 		assert_that( results, has_length( 0 ) )
+# 		#sj = 'sjohnson@nextthought.com'
+# 		
+# 		# ComponentLookupError: (<InterfaceClass zope.intid.interfaces.IIntIds>, '')?
+# 		# Mock DS does not have this component?
+# 		#init_db( self.db, usernames=[sj] )
+# 		
+# 		uid = 01
+# 		entity = User.create_user( self.ds, 'foo@bar' )
+# 		init( uid, self.db, entity )		
+# 		
+# 		session = self.db.get_session()
+# 		results = session.query(Users).all()
+# 		assert_that( results, has_length( 1 ) )
+
+class TestImport(nti.testing.base.ConfiguringTestBase):
+	
+	def setUp(self):
+		self.db = AnalyticsDB( dburi='sqlite://' )
+		self.session = self.db.get_session()
+		self.configure_string(ZCML_STRING)
+	
+	def tearDown(self):
+		self.session.close()
+	
+	def test_import(self):
+		results = self.session.query(Users).all()
+		assert_that( results, has_length( 0 ) )
+		
+		uid = 1
+		init( uid, self.db, User( username='test@nextthought' ) )		
+		
+		results = self.session.query(Users).all()
+		assert_that( results, has_length( 1 ) )
+		
+		new_user = self.session.query(Users).one()
+		assert_that( new_user, has_property( 'user_ds_id', uid ) )
+		assert_that( new_user, has_property( 'user_id' ), 1 )
+		
---
+++ src/nti/analytics/database/tests/test_zcml.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import is_not
+from hamcrest import not_none
+from hamcrest import assert_that
+from hamcrest import has_property
+
+from zope import component
+
+from nti.analytics.database.interfaces import IAnalyticsDB
+
+from nti.analytics import get_analytics_db
+
+import nti.testing.base
+
+ZCML_STRING = """
+<configure	xmlns="http://namespaces.zope.org/zope"
+			xmlns:i18n="http://namespaces.zope.org/i18n"
+			xmlns:zcml="http://namespaces.zope.org/zcml"
+			xmlns:adb="http://nextthought.com/analytics/database">
+
+	<include package="zope.component" file="meta.zcml" />
+	<include package="zope.security" file="meta.zcml" />
+	<include package="zope.component" />
+	<include package="." file="meta.zcml" />
+			 		 
+	<configure>
+		<adb:registerAnalyticsDB 	defaultSQLite="True"
+									name="janux.ou.edu"
+									twophase="True"
+									autocommit="False" />
+	</configure>
+</configure>
+
+"""
+
+class TestZcml(nti.testing.base.ConfiguringTestBase):
+
+	def test_registration(self):
+		self.configure_string(ZCML_STRING)
+
+		db = component.queryUtility(IAnalyticsDB)
+		assert_that(db, none())
+		
+		db = component.queryUtility(IAnalyticsDB, name='janux.ou.edu')
+		assert_that(db, not_none())
+		
+		db = get_analytics_db()
+		assert_that(db, none())
+		
+		db = get_analytics_db( names=['janux.ou.edu'] )
+		assert_that(db, not_none())
+		assert_that( db, has_property('twophase', True ) )
+		assert_that( db, has_property('autocommit', False ) )
