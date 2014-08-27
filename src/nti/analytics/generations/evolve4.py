6fc3dd8c9ef51c752ba8862edb466bc3abf7ae1a
Josh Zuech <josh.zuech@nextthought.com>
2014-08-27 15:01:23 +0000

    Fix evolve
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@48167 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics/generations/evolve4.py
+++ src/nti/analytics/generations/evolve4.py
@@ -10,6 +10,12 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import zope.intid
+from zope import component
+from zope.component.hooks import site, setHooks
+
+from nti.async.interfaces import IQueue
+
 generation = 4
 
 QUEUE_NAME = '++etc++analytics++queue'
