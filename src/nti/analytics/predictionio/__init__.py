dcea6172ef6f4c2564ed2f54c16206532fbc7e17
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-03 01:16:05 +0000

    fix constructor call
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@60665 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics/predictionio/__init__.py
+++ src/nti/analytics/predictionio/__init__.py
@@ -54,7 +54,8 @@ get_current_user = get_user
 def get_predictionio_client(client=None, name=''):
 	if client is None:
 		app = get_predictionio_app(name=name)
-		client = EventClient(app.AppKey, apiurl=app.URL) if app is not None else None
+		client = EventClient(access_key=app.AppKey, url=app.URL) \
+				 if app is not None else None
 	return client
 
 def object_finder(obj):
