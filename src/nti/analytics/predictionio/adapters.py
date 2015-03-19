6b0926d1770a3cda0dd560127d59bf1bc07bd9d8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-19 20:53:17 +0000

    add adapter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@61650 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics/predictionio/adapters.py
+++ src/nti/analytics/predictionio/adapters.py
@@ -21,6 +21,8 @@ from nti.contentlibrary.interfaces import IContentUnit
 
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
+from nti.contenttypes.presentation.interfaces import INTIIDIdentifiable
+
 from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import INote
 from nti.dataserver.interfaces import IModeledContent
@@ -159,4 +161,10 @@ def _CourseCatalogEntryOIDAdpater(item):
 @component.adapter(IUser)
 def _UserOIDAdpater(user):
 	result = user.username
+	return result
+
+@interface.implementer(IOID)
+@component.adapter(INTIIDIdentifiable)
+def _NTIIDIdentifiableOIDAdpater(item):
+	result = item.ntiid
 	return result
