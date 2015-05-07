270b55735347daf67fc2e99d7f33e290202aa27b
Josh Zuech <josh.zuech@nextthought.com>
2015-05-07 22:56:31 +0000

    Allow the resource_id col to be null in assessment views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@64724 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/analytics/generations/evolve30.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+generation 30.
+
+.. $Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 30
+
+from zope.component.hooks import setHooks
+
+from sqlalchemy import Integer
+
+from alembic.operations import Operations
+from alembic.migration import MigrationContext
+
+from nti.analytics.database import get_analytics_db
+from nti.analytics.database.assessments import AssignmentViews
+from nti.analytics.database.assessments import SelfAssessmentViews
+
+TABLES = [ AssignmentViews, SelfAssessmentViews ]
+
+def do_evolve():
+	setHooks()
+	db = get_analytics_db()
+
+	if db.defaultSQLite:
+		# sqlite does not let us alter columns.
+		return
+
+	# Cannot use transaction with alter table scripts and mysql
+	connection = db.engine.connect()
+	mc = MigrationContext.configure( connection )
+	op = Operations(mc)
+
+	for table in TABLES:
+		op.alter_column( table.__tablename__, 'resource_id', existing_type=Integer, nullable=True )
+
+	logger.info( 'Finished analytics evolve (%s)', generation )
+
+def evolve(_):
+	"""
+	Make our resource_id column nullable.
+	"""
+	do_evolve()
