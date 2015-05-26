b9b162d2c726d51e49106dc76a84f18370586e87
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-26 01:28:47 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@65997 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics/utils/constructor.py
+++ src/nti/analytics/utils/constructor.py
@@ -3,6 +3,7 @@
 """
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
@@ -13,6 +14,7 @@ import logging
 from zope import component
 
 from zope.container.contained import Contained
+
 from z3c.autoinclude.zcml import includePluginsDirective
 
 from nti.async.utils.processor import Processor
@@ -20,10 +22,10 @@ from nti.async.utils.processor import Processor
 from nti.analytics import QUEUE_NAMES
 from nti.analytics.interfaces import IObjectProcessor
 
-from nti.analytics.resource_views import logger as resource_view_logger
 from nti.analytics.users import logger as users_logger
 from nti.analytics.sessions import logger as sessions_logger
 from nti.analytics.resolvers import logger as resolvers_logger
+from nti.analytics.resource_views import logger as resource_view_logger
 
 class PluginPoint(Contained):
 
@@ -39,18 +41,17 @@ class Constructor(Processor):
 		if args.verbose:
 			for _, module in component.getUtilitiesFor(IObjectProcessor):
 				module.logger.setLevel(logging.DEBUG)
-			resource_view_logger.setLevel( logging.DEBUG )
-			users_logger.setLevel( logging.DEBUG )
-			sessions_logger.setLevel( logging.DEBUG )
-			resolvers_logger.setLevel( logging.DEBUG )
+			for logger in (resource_view_logger, users_logger, 
+						   sessions_logger, resolvers_logger):
+				logger.setLevel( logging.DEBUG )
 
 	def extend_context(self, context):
 		includePluginsDirective(context, PP_ANALYTICS)
 
 	def process_args(self, args):
+		setattr(args, 'redis', True)
 		setattr(args, 'library', True)  # load library
 		setattr(args, 'queue_names', QUEUE_NAMES)
-		setattr(args, 'redis', True)
 		super(Constructor, self).process_args(args)
 
 def main():
