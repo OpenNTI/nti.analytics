f014c5d0a1c784c134db571a676505a9063c60a6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-11-29 06:08:33 +0000

    fix code imports
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@77864 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics/enrollments.py
+++ src/nti/analytics/enrollments.py
@@ -1,111 +1,114 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 """
-$Id$
+.. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from datetime import datetime
+
 from zope import component
 
-from nti.ntiids import ntiids
+from nti.contenttypes.courses.enrollment import DefaultPrincipalEnrollments
+from nti.contenttypes.courses.interfaces import ICourseInstanceEnrollmentRecord
+
+from nti.dataserver.interfaces import IUser
 
 from nti.intid.interfaces import IIntIdAddedEvent
 from nti.intid.interfaces import IIntIdRemovedEvent
 
-from nti.dataserver.interfaces import IUser
+from nti.ntiids import ntiids
 
-from nti.contenttypes.courses.interfaces import ICourseInstanceEnrollmentRecord
-from nti.contenttypes.courses.enrollment import DefaultPrincipalEnrollments
+from .common import get_entity
+from .common import process_event
+from .common import get_root_context_name
 
-from datetime import datetime
+from .database import enrollments as db_enrollments
 
-from nti.analytics import get_factory
-from nti.analytics import ENROLL_ANALYTICS
+from .interfaces import IObjectProcessor
 
-from nti.analytics.interfaces import IObjectProcessor
+from .sessions import get_nti_session_id
 
-from nti.analytics.sessions import get_nti_session_id
-
-from nti.analytics.database import enrollments as db_enrollments
-
-from nti.analytics.common import get_root_context_name
-from nti.analytics.common import process_event
-from nti.analytics.common import get_entity
+from . import get_factory
+from . import ENROLL_ANALYTICS
 
 get_enrollments_for_course = db_enrollments.get_enrollments_for_course
 
 def _get_job_queue():
 	factory = get_factory()
-	return factory.get_queue( ENROLL_ANALYTICS )
+	return factory.get_queue(ENROLL_ANALYTICS)
 
-def _add_drop( oid, username, scope, nti_session=None, timestamp=None ):
-	course = ntiids.find_object_with_ntiid( oid )
-	user = get_entity( username )
+def _add_drop(oid, username, scope, nti_session=None, timestamp=None):
+	course = ntiids.find_object_with_ntiid(oid)
+	user = get_entity(username)
 	if 		user is not None \
 		and course is not None:
 
-		course_name = get_root_context_name( course )
-		db_enrollments.create_course_drop( user, nti_session, timestamp, course )
-		logger.debug( "User dropped (user=%s) (course=%s)",
-					user,
-					course_name )
+		course_name = get_root_context_name(course)
+		db_enrollments.create_course_drop(user, nti_session, timestamp, course)
+		logger.debug("User dropped (user=%s) (course=%s)",
+					 user,
+					 course_name)
 
-def _add_enrollment( oid, username, scope, nti_session=None, timestamp=None ):
-	course = ntiids.find_object_with_ntiid( oid )
-	user = get_entity( username )
+def _add_enrollment(oid, username, scope, nti_session=None, timestamp=None):
+	course = ntiids.find_object_with_ntiid(oid)
+	user = get_entity(username)
 	if 		user is not None \
 		and course is not None:
 
 		enrollment_type = scope
-		course_name = get_root_context_name( course )
-		db_enrollments.create_course_enrollment( user, nti_session, timestamp, course, enrollment_type )
-		logger.debug( "User enrollment (user=%s) (course=%s) (type=%s)",
-					user,
-					course_name,
-					enrollment_type )
+		course_name = get_root_context_name(course)
+		db_enrollments.create_course_enrollment(user, nti_session, timestamp,
+												course, enrollment_type)
+		logger.debug("User enrollment (user=%s) (course=%s) (type=%s)",
+					 user,
+					 course_name,
+					 enrollment_type)
 
-def _handle_event( record, to_call ):
+def _handle_event(record, to_call):
 	timestamp = datetime.utcnow()
 	user = record.Principal
-	username = getattr( user, 'username', None )
+	username = getattr(user, 'username', None)
 	course = record.CourseInstance
 	scope = record.Scope
 
 	nti_session = get_nti_session_id()
-	process_event( _get_job_queue,
-					to_call,
-					course,
-					username=username,
-					scope=scope,
-					timestamp=timestamp,
-					nti_session=nti_session )
+	process_event(_get_job_queue,
+				  to_call,
+				  course,
+				  username=username,
+				  scope=scope,
+				  timestamp=timestamp,
+				  nti_session=nti_session)
 
 
-@component.adapter( ICourseInstanceEnrollmentRecord, IIntIdAddedEvent )
-def _enrolled( record, event ):
+@component.adapter(ICourseInstanceEnrollmentRecord, IIntIdAddedEvent)
+def _enrolled(record, event):
 	# TODO This does not handle if we manually migrate users from
 	# one super course to many sectioned courses.
-	_handle_event( record, _add_enrollment )
+	_handle_event(record, _add_enrollment)
 
-@component.adapter( ICourseInstanceEnrollmentRecord, IIntIdRemovedEvent )
-def _dropped( record, event ):
-	_handle_event( record, _add_drop )
+@component.adapter(ICourseInstanceEnrollmentRecord, IIntIdRemovedEvent)
+def _dropped(record, event):
+	_handle_event(record, _add_drop)
 
-def _user_enrollments( user ):
-	enrollments = DefaultPrincipalEnrollments( user )
+def _user_enrollments(user):
+	enrollments = DefaultPrincipalEnrollments(user)
 	for enrollment in enrollments.iter_enrollments():
 		course = enrollment.CourseInstance
 		username = user.username
 		scope = enrollment.Scope
-		process_event( _get_job_queue, _add_enrollment, course, username=username, scope=scope )
+		process_event(_get_job_queue, _add_enrollment, course,
+					  username=username, scope=scope)
 
-component.moduleProvides( IObjectProcessor )
-def init( obj ):
+component.moduleProvides(IObjectProcessor)
+def init(obj):
 	result = False
 	if 	IUser.providedBy(obj):
-		_user_enrollments( obj )
+		_user_enrollments(obj)
 		result = True
 	return result
--- src/nti/analytics/identifier.py
+++ src/nti/analytics/identifier.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 """
-$Id$
+.. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import zope.intid
+from six import string_types
 
-from zope import interface
+from zope.intid import IIntIds
+
 from zope import component
+from zope import interface
 
 from ZODB.interfaces import IBroken
 from ZODB.POSException import POSError
@@ -20,95 +23,92 @@ from nti.analytics_database.interfaces import IAnalyticsIntidIdentifier
 from nti.analytics_database.interfaces import IAnalyticsNTIIDIdentifier
 from nti.analytics_database.interfaces import IAnalyticsRootContextIdentifier
 
-from nti.ntiids import ntiids
-
-from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
-from six import string_types
+from nti.ntiids.ntiids import find_object_with_ntiid
 
-@interface.implementer( IAnalyticsIntidIdentifier )
-class _DSIdentifier( object ):
+@interface.implementer(IAnalyticsIntidIdentifier)
+class _DSIdentifier(object):
 
-	def _get_intid_utility( self ):
-		intids = component.getUtility( zope.intid.IIntIds )
+	def _get_intid_utility(self):
+		intids = component.getUtility(IIntIds)
 		return intids
 
-	def get_id( self, obj ):
-		result = getattr( obj, '_ds_intid', None )
-		return result or self._get_intid_utility().getId( obj )
+	def get_id(self, obj):
+		result = getattr(obj, '_ds_intid', None)
+		return result or self._get_intid_utility().getId(obj)
 
-	def get_object( self, uid ):
+	def get_object(self, uid):
 		result = None
 		try:
-			obj = self._get_intid_utility().queryObject( uid, default=None )
-			if not IBroken.providedBy( obj ):
+			obj = self._get_intid_utility().queryObject(uid, default=None)
+			if not IBroken.providedBy(obj):
 				result = obj
 		except (TypeError, POSError):
-			logger.warn( 'Broken object missing (id=%s)', uid )
+			logger.warn('Broken object missing (id=%s)', uid)
 		return result
 
-@interface.implementer( IAnalyticsNTIIDIdentifier )
-class _NTIIDIdentifier( object ):
+@interface.implementer(IAnalyticsNTIIDIdentifier)
+class _NTIIDIdentifier(object):
 
-	def get_id( self, resource ):
+	def get_id(self, resource):
 		""" Resource could be a video or content piece. """
-		if isinstance( resource, string_types ):
+		if isinstance(resource, string_types):
 			result = resource
 		else:
-			result = getattr( resource, 'ntiid', None )
+			result = getattr(resource, 'ntiid', None)
 		return result
 
-	def get_object( self, uid ):
-		return ntiids.find_object_with_ntiid( uid )
+	def get_object(self, uid):
+		return find_object_with_ntiid(uid)
 
-@interface.implementer( IAnalyticsRootContextIdentifier )
-class _RootContextIdentifier( object ):
+@interface.implementer(IAnalyticsRootContextIdentifier)
+class _RootContextIdentifier(object):
 
-	def get_id( self, root_context ):
+	def get_id(self, root_context):
 		""" Could be a course or content-package."""
 		# TODO It seems external OID would be preferrable,
 		# perhaps much faster lookups.
-		catalog_entry = ICourseCatalogEntry( root_context, None )
-		result = getattr( catalog_entry, 'ntiid', None )
+		catalog_entry = ICourseCatalogEntry(root_context, None)
+		result = getattr(catalog_entry, 'ntiid', None)
 
 		if result is None:
 			# Legacy course or content
-			result = getattr( root_context, 'ContentPackageNTIID',
-						getattr( root_context, 'ntiid', None ) )
+			result = getattr(root_context, 'ContentPackageNTIID',
+							 getattr(root_context, 'ntiid', None))
 		return result
 
-	def get_object( self, ntiid ):
-		obj = ntiids.find_object_with_ntiid( ntiid )
+	def get_object(self, ntiid):
+		obj = find_object_with_ntiid(ntiid)
 		# We may have:
 		# 1. content package -> legacy course
 		# 2. catalog entry -> new course
 		# 3. content package / book
 		# Adapt for 1,2; return 3.
-		obj = ICourseInstance( obj, obj )
+		obj = ICourseInstance(obj, obj)
 		return obj
 
-def get_ds_object( obj_id ):
-	id_utility = component.getUtility( IAnalyticsIntidIdentifier )
-	return id_utility.get_object( obj_id )
+def get_ds_object(obj_id):
+	id_utility = component.getUtility(IAnalyticsIntidIdentifier)
+	return id_utility.get_object(obj_id)
 
-def get_ntiid_object( obj_id ):
-	id_utility = component.getUtility( IAnalyticsNTIIDIdentifier )
-	return id_utility.get_object( obj_id )
+def get_ntiid_object(obj_id):
+	id_utility = component.getUtility(IAnalyticsNTIIDIdentifier)
+	return id_utility.get_object(obj_id)
 
-def get_root_context_object( obj_id ):
-	id_utility = component.getUtility( IAnalyticsRootContextIdentifier )
-	return id_utility.get_object( obj_id )
+def get_root_context_object(obj_id):
+	id_utility = component.getUtility(IAnalyticsRootContextIdentifier)
+	return id_utility.get_object(obj_id)
 
-def get_ds_id( obj ):
-	id_utility = component.getUtility( IAnalyticsIntidIdentifier )
-	return id_utility.get_id( obj )
+def get_ds_id(obj):
+	id_utility = component.getUtility(IAnalyticsIntidIdentifier)
+	return id_utility.get_id(obj)
 
-def get_ntiid_id( obj ):
-	id_utility = component.getUtility( IAnalyticsNTIIDIdentifier )
-	return id_utility.get_id( obj )
+def get_ntiid_id(obj):
+	id_utility = component.getUtility(IAnalyticsNTIIDIdentifier)
+	return id_utility.get_id(obj)
 
-def get_root_context_id( obj ):
-	id_utility = component.getUtility( IAnalyticsRootContextIdentifier )
-	return id_utility.get_id( obj )
-
+def get_root_context_id(obj):
+	id_utility = component.getUtility(IAnalyticsRootContextIdentifier)
+	return id_utility.get_id(obj)
