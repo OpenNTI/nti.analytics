905a3d6a4301c1c376a5ef2789d9ec31c2804452
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-01-25 20:43:59 +0000

    fix evolve
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@81696 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics/generations/evolve3.py
+++ src/nti/analytics/generations/evolve3.py
@@ -12,16 +12,18 @@ logger = __import__('logging').getLogger(__name__)
 
 generation = 3
 
-import zope.intid
 from zope.component.hooks import site, setHooks
+
+from zope.intid.interfaces import IIntIds
+
+from nti.analytics import QUEUE_NAME
+from nti.analytics import TOPICS_ANALYTICS
+from nti.analytics import COMMENTS_ANALYTICS
 
 from nti.async import queue
 from nti.async.interfaces import IQueue
 
-from nti.analytics import FAIL_QUEUE
-from nti.analytics import TOPICS_ANALYTICS
-from nti.analytics import COMMENTS_ANALYTICS
-
+FAIL_QUEUE = QUEUE_NAME + '++failure'
 QUEUE_NAMES = [ FAIL_QUEUE, TOPICS_ANALYTICS, COMMENTS_ANALYTICS ]
 
 def do_evolve(context):
@@ -32,16 +34,16 @@ def do_evolve(context):
 
 	with site(ds_folder):
 		lsm = ds_folder.getSiteManager()
-		intids = lsm.getUtility(zope.intid.IIntIds)
+		intids = lsm.getUtility(IIntIds)
 
 		for new_queue_name in QUEUE_NAMES:
 			result = queue.Queue()
 			result.__parent__ = ds_folder
 			result.__name__ = new_queue_name
-			intids.register( result )
-			lsm.registerUtility( result, provided=IQueue, name=new_queue_name )
+			intids.register(result)
+			lsm.registerUtility(result, provided=IQueue, name=new_queue_name)
 
-	logger.info( 'Finished analytics evolve3' )
+	logger.info('Finished analytics evolve3')
 
 def evolve(context):
 	"""
