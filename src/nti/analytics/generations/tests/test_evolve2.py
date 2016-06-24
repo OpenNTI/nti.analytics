935edf32aa434f69d4cd36c65f70484583fcfabf
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-06-24 12:11:28 +0000

    fix test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@91160 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics/generations/tests/test_evolve2.py
+++ src/nti/analytics/generations/tests/test_evolve2.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-$Id$
-"""
-from __future__ import print_function, unicode_literals
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
 from hamcrest import assert_that
 
 from nti.testing.matchers import verifiably_provides
 
 from zope import component
+
 from zope.component.hooks import site
-
-import nti.dataserver.tests.mock_dataserver as mock_dataserver
-from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
-
-from nti.async import queue
-from nti.async.interfaces import IQueue
 
 from nti.analytics.tests import NTIAnalyticsTestCase
 from nti.analytics.generations import evolve2
 
-from nti.analytics.generations.evolve2 import QUEUE_NAMES
 from nti.analytics import QUEUE_NAME as LEGACY_QUEUE_NAME
+from nti.analytics.generations.evolve2 import QUEUE_NAMES
+
+from nti.async import queue
+from nti.async.interfaces import IQueue
+
+import nti.dataserver.tests.mock_dataserver as mock_dataserver
+from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
 
 class TestEvolve2(NTIAnalyticsTestCase):
 
@@ -30,7 +33,8 @@ class TestEvolve2(NTIAnalyticsTestCase):
 	def test_evolve2(self):
 
 		conn = mock_dataserver.current_transaction
-		class _context(object): pass
+		class _context(object): 
+			pass
 		context = _context()
 		context.connection = conn
 
@@ -41,15 +45,11 @@ class TestEvolve2(NTIAnalyticsTestCase):
 		old_queue.__parent__ = ds_folder
 		old_queue.__name__ = LEGACY_QUEUE_NAME
 		lsm = ds_folder.getSiteManager()
-		lsm.registerUtility( old_queue, provided=IQueue, name=LEGACY_QUEUE_NAME )
+		lsm.registerUtility(old_queue, provided=IQueue, name=LEGACY_QUEUE_NAME)
 
-		evolve2.do_evolve( context )
+		evolve2.do_evolve(context)
 
-		with site( ds_folder ):
-			# Not sure why this doesn't work
-# 			legacy_queue = lsm.queryUtility( IQueue, name=LEGACY_QUEUE_NAME )
-# 			assert_that( legacy_queue, none() )
-
+		with site(ds_folder):
 			for queue_name in QUEUE_NAMES:
-				job_queue = component.getUtility( IQueue, name=queue_name )
-				assert_that( job_queue, verifiably_provides( IQueue ) )
+				job_queue = component.getUtility(IQueue, name=queue_name)
+				assert_that(job_queue, verifiably_provides(IQueue))
