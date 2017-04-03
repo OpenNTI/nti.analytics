4d2488fb6613ec90a9de4928839fa52a28f6be0f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-03 21:48:39 +0000

    Context loads everything
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics/trunk@110080 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics/utils/constructor.py
+++ src/nti/analytics/utils/constructor.py
@@ -31,37 +31,47 @@ from nti.analytics.sessions import logger as sessions_logger
 
 from nti.analytics.users import logger as users_logger
 
+from nti.dataserver.utils.base_script import create_context
+
+
 class PluginPoint(Contained):
 
-	def __init__(self, name):
-		self.__name__ = name
-
+    def __init__(self, name):
+        self.__name__ = name
 PP_ANALYTICS = PluginPoint('nti.analytics')
 PP_ANALYTICS_GRAPHDB = PluginPoint('nti.analytics_graphdb')
 
+
 class Constructor(Processor):
 
-	def set_log_formatter(self, args):
-		super(Constructor, self).set_log_formatter(args)
-		if args.verbose:
-			for _, module in component.getUtilitiesFor(IObjectProcessor):
-				module.logger.setLevel(logging.DEBUG)
-			for logger in (resource_view_logger, users_logger, 
-						   sessions_logger, resolvers_logger):
-				logger.setLevel( logging.DEBUG )
+    def set_log_formatter(self, args):
+        super(Constructor, self).set_log_formatter(args)
+        if args.verbose:
+            for _, module in component.getUtilitiesFor(IObjectProcessor):
+                module.logger.setLevel(logging.DEBUG)
+            for logger in (resource_view_logger, users_logger,
+                           sessions_logger, resolvers_logger):
+                logger.setLevel(logging.DEBUG)
 
-	def extend_context(self, context):
-		includePluginsDirective(context, PP_ANALYTICS)
-		includePluginsDirective(context, PP_ANALYTICS_GRAPHDB)
+    def extend_context(self, context):
+        includePluginsDirective(context, PP_ANALYTICS)
+        includePluginsDirective(context, PP_ANALYTICS_GRAPHDB)
 
-	def process_args(self, args):
-		setattr(args, 'redis', True)
-		setattr(args, 'library', True)  # load library
-		setattr(args, 'queue_names', QUEUE_NAMES)
-		super(Constructor, self).process_args(args)
+    def create_context(self, env_dir, args=None):
+        context = create_context(env_dir, with_library=True)
+        self.extend_context(context)
+        return context
+
+    def process_args(self, args):
+        setattr(args, 'redis', True)
+        setattr(args, 'library', True)  # load library
+        setattr(args, 'queue_names', QUEUE_NAMES)
+        super(Constructor, self).process_args(args)
+
 
 def main():
-	return Constructor()()
+    return Constructor()()
+
 
 if __name__ == '__main__':
-	main()
+    main()
