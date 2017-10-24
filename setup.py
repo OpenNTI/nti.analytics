66e7e985fa69bd4336f7bd402d29ae774eecbac2
Carlos Sanchez <papachoco@gmail.com>
2017-10-24 08:59:57 -0500

    fix console_scripts

--- setup.py
+++ setup.py
@@ -4,47 +4,49 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	'console_scripts': [
-		"nti_analytics_migrator = nti.analytics.utils.ds_migrator:main",
-		"nti_analytics_event_uploader = nti.analytics.utils.event_uploader:main"
-		"nti_analytics_video_duration = nti.analytics.utils.upload_video_durations:main",
-	],
+    'console_scripts': [
+        "nti_analytics_migrator = nti.analytics.utils.ds_migrator:main",
+        "nti_analytics_event_uploader = nti.analytics.utils.event_uploader:main",
+        "nti_analytics_video_duration = nti.analytics.utils.upload_video_durations:main",
+    ],
 }
 
 setup(
-	name='nti.analytics',
-	version=VERSION,
-	author='Josh Zuech',
-	author_email='josh.zuech@nextthought.com',
-	description="NTI Analytics",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='pyramid preference',
-	classifiers=[
-		'Intended Audience :: Developers',
+    name='nti.analytics',
+    version=VERSION,
+    author='Josh Zuech',
+    author_email='josh.zuech@nextthought.com',
+    description="NTI Analytics",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='pyramid preference',
+    classifiers=[
+        'Intended Audience :: Developers',
 		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
 		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti'],
-	install_requires=[
-		'setuptools',
-		'alembic',
-		'geopy',
-		'python-geoip',
-		'python-geoip-geolite2',
-		'sqlalchemy',
-		'user-agents',
+		'Programming Language :: Python :: Implementation :: CPython',
+		'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti'],
+    install_requires=[
+        'setuptools',
+        'alembic',
+        'geopy',
 		'nti.analytics_database',
 		'nti.app.assessment',
 		'nti.app.products.courseware',
 		'nti.app.products.courseware_ims',
 		'nti.app.products.gradebook',
 		'nti.async',
-		'nti.namedfile'
-	],
-	entry_points=entry_points
+		'nti.namedfile',
+        'python-geoip',
+        'python-geoip-geolite2',
+        'sqlalchemy',
+        'user-agents',
+    ],
+    entry_points=entry_points
 )
