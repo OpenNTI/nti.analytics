9b230ca754be4167ad548041a0edb3baff8d37d1
Carlos Sanchez <papachoco@gmail.com>
2017-12-01 13:52:32 -0600

    pylint; enable generation 47

--- src/nti/analytics/generations/install.py
+++ src/nti/analytics/generations/install.py
@@ -4,24 +4,28 @@ schema generation installation.
 
 .. $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-generation = 46
+generation = 47
 
 from zope.generations.generations import SchemaManager
 
-class _AnalyticsSchemaManager(SchemaManager):
-	"""
-	A schema manager that we can register as a utility in ZCML.
-	"""
-	def __init__(self):
-		super(_AnalyticsSchemaManager, self).__init__(
-											generation=generation,
-											minimum_generation=generation,
-											package_name='nti.analytics.generations')
-def evolve(context):
-	pass
+logger = __import__('logging').getLogger(__name__)
 
+class _AnalyticsSchemaManager(SchemaManager):
+    """
+    A schema manager that we can register as a utility in ZCML.
+    """
+
+    def __init__(self):
+        super(_AnalyticsSchemaManager, self).__init__(
+            generation=generation,
+            minimum_generation=generation,
+            package_name='nti.analytics.generations')
+
+
+def evolve(unused_context):
+    pass
