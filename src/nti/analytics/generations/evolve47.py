5c1d92c873ce2c704a9f340d806021a948dc8e60
Carlos Sanchez <papachoco@gmail.com>
2017-11-30 21:40:50 -0600

    set root folder in mock ds

--- src/nti/analytics/generations/evolve47.py
+++ src/nti/analytics/generations/evolve47.py
@@ -15,7 +15,9 @@ import pickle
 from io import BytesIO
 
 from zope import component
+from zope import interface
 
+from zope.component.hooks import getSite
 from zope.component.hooks import setHooks
 from zope.component.hooks import site as current_site
 
@@ -23,6 +25,8 @@ from nti.analytics import QUEUE_NAMES
 
 from nti.contentlibrary.interfaces import IContentPackageLibrary
 
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
 from nti.dataserver.interfaces import IRedisClient
 
 generation = 47
@@ -38,11 +42,11 @@ def _unpickle(data):
     return result
 
 
-def _reset(redis, name, hash_key):
-    keys = redis.pipeline().delete(name) \
-                .hkeys(hash_key).execute()
+def _reset(redis_client, name, hash_key):
+    keys = redis_client.pipeline().delete(name) \
+                       .hkeys(hash_key).execute()
     if keys and keys[1]:
-        redis.hdel(hash_key, *keys[1])
+        redis_client.hdel(hash_key, *keys[1])
         return keys[1]
     return ()
 
@@ -53,34 +57,57 @@ def _load_library():
         library.syncContentPackages()
 
 
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+    root = None
+    root_folder = None
+
+    def get_by_oid(self, oid, ignore_creator=False):
+        resolver = component.queryUtility(IOIDResolver)
+        if resolver is None:
+            logger.warn("Using dataserver without a proper ISiteManager.")
+        else:
+            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+        return None
+
+
 def do_evolve(context, generation=generation):
     setHooks()
     conn = context.connection
     ds_folder = conn.root()['nti.dataserver']
+    redis_client = component.getUtility(IRedisClient)
+
+    mock_ds = MockDataserver()
+    mock_ds.root = ds_folder
+    component.provideUtility(mock_ds, IDataserver)
 
     with current_site(ds_folder):
         assert component.getSiteManager() == ds_folder.getSiteManager(), \
                "Hooks not installed?"
 
+        # set root folder
+        mock_ds.root_folder = getSite().__parent__
+        
         _load_library()
 
-        _redis = component.queryUtility(IRedisClient)
         for name in QUEUE_NAMES:
             # process jobs
             hash_key = name + '/hash'
-            data = _redis.lrange(name, 0, -1)
+            data = redis_client.lrange(name, 0, -1)
             for job in (_unpickle(x) for x in data or ()):
                 try:
                     job()
                 except Exception:
                     logger.error("Cannot execute analytics job %s", job)
-            _reset(_redis, name, hash_key)
+            _reset(redis_client, name, hash_key)
 
             # reset failed 
             name += "/failed"
             hash_key = name + '/hash'
-            _reset(_redis, name, hash_key)
+            _reset(redis_client, name, hash_key)
 
+    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
     logger.info('Analytics evolution %s done', generation)
 
 
