9c5b1945b4ce8e35367f7a09047efbb6b4075793
Josh Zuech <josh.zuech@nextthought.com>
2017-12-07 21:03:35 -0600

    Adapt on Sessions db record

--- src/nti/analytics/locations.py
+++ src/nti/analytics/locations.py
@@ -12,10 +12,10 @@ from zope import component
 from zope import interface
 
 from nti.analytics_database.sessions import Location
+from nti.analytics.database.sessions import Sessions
 
 from nti.analytics.database import locations as db_locations
 
-from nti.analytics.interfaces import IAnalyticsSession
 from nti.analytics.interfaces import IGeographicalLocation
 
 from nti.analytics.model import GeographicalLocation
@@ -24,8 +24,8 @@ update_missing_locations = db_locations.update_missing_locations
 get_location_list = db_locations.get_location_list
 location_for_ip = db_locations.location_for_ip
 
-@component.adapter( Location )
-@interface.implementer( IGeographicalLocation )
+@component.adapter(Location)
+@interface.implementer(IGeographicalLocation)
 def _from_db_location(db_location):
     kwargs = {}
     for db_attr, attr in (('latitude', 'Latitude'),
@@ -36,8 +36,8 @@ def _from_db_location(db_location):
         kwargs[attr] = getattr(db_location, db_attr, None)
     return GeographicalLocation(**kwargs)
 
-@component.adapter( IAnalyticsSession )
-@interface.implementer( IGeographicalLocation )
+@component.adapter(Sessions)
+@interface.implementer(IGeographicalLocation)
 def _location_for_session(session):
     location = location_for_ip(session.ip_addr)
     return IGeographicalLocation(location) if location else None
