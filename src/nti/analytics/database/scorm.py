b3645a02def2c76bc725fa532e0a09b5fa8a1240
Unknown <zinth@me.com>
2018-04-05 17:21:36 -0500

    Declare get_launch_records_for_ntiid.

--- src/nti/analytics/database/scorm.py
+++ src/nti/analytics/database/scorm.py
@@ -10,12 +10,16 @@ from __future__ import absolute_import
 
 from nti.analytics.common import timestamp_type
 
+from nti.analytics.database import resolve_objects
 from nti.analytics.database import get_analytics_db
 
 from nti.analytics.database._utils import get_context_path
 from nti.analytics.database._utils import get_root_context_ids
 
+from nti.analytics.database.query_utils import get_filtered_records
+
 from nti.analytics.database.resources import get_resource_id
+from nti.analytics.database.resources import get_resource_record
 
 from nti.analytics.database.users import get_or_create_user
 
@@ -47,4 +51,29 @@ def create_launch_record(user, course, metadata, nti_session, context_path, time
                                     resource_id=rid,
                                     time_length=None)
     db.session.add(new_object)
+    
+    
+def _resolve_launch_record(record, root_context=None, user=None):
+    if root_context is not None:
+        record.RootContext = root_context
+    if user is not None:
+        record.user = user
+    return record
+    
+    
+def get_launch_records_for_ntiid(metadata_ntiid, user=None, root_context=None, **kwargs):
+    results = ()
+    db = get_analytics_db()
+    resource_record = get_resource_record(db, metadata_ntiid)
+    if resource_record is not None:
+        resource_id = resource_record.resource_id
+        filters = (SCORMResourceViews.resource_id == resource_id)
+        launch_records = get_filtered_records(user,
+                                              SCORMResourceViews,
+                                              root_context=root_context,
+                                              filters=filters,
+                                              **kwargs)
+        results = resolve_objects(_resolve_launch_record, launch_records,
+                                  user=user, root_context=root_context)
+    return results
     
