17b1f038f36b706548e2d5ad4cacdc614f68ff98
Unknown <zinth@me.com>
2018-04-11 12:29:17 -0500

    Test query kwargs.

--- src/nti/analytics/tests/test_scorm.py
+++ src/nti/analytics/tests/test_scorm.py
@@ -9,12 +9,15 @@ import zope.intid
 
 from datetime import datetime
 
+from hamcrest import is_
 from hamcrest import none
 from hamcrest import is_not
 from hamcrest import has_length
 from hamcrest import assert_that
 
 from zope import component
+
+from nti.analytics.common import timestamp_type
 
 from nti.analytics.database import scorm as db_scorm
 
@@ -33,8 +36,8 @@ from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
 test_user_ds_id = MockUser(u'78')
 
 
-def _create_scorm_package_launch_event(user, course, metadata_ntiid, context_path):
-    db_scorm.create_launch_record(user, course, metadata_ntiid, test_session_id, context_path, datetime.min)
+def _create_scorm_package_launch_event(user, course, metadata_ntiid, context_path, timestamp):
+    db_scorm.create_launch_record(user, course, metadata_ntiid, test_session_id, context_path, timestamp)
 
 
 class TestSCORMResourceViews(NTIAnalyticsTestCase):
@@ -48,7 +51,8 @@ class TestSCORMResourceViews(NTIAnalyticsTestCase):
         intids.register(course)
         metadata_ntiid = u'tag:metadata_id'
         assert_that(metadata_ntiid, is_not(none()))
-        _create_scorm_package_launch_event(user, course, metadata_ntiid, [course_ntiid])
+        timestamp_min = timestamp_type(datetime.min)
+        _create_scorm_package_launch_event(user, course, metadata_ntiid, [course_ntiid], timestamp_min)
     
         # Empty
         results = get_scorm_package_launches_for_ntiid(metadata_ntiid + u'dne')
@@ -60,3 +64,23 @@ class TestSCORMResourceViews(NTIAnalyticsTestCase):
         assert_that(results, has_length(1))
         results = get_scorm_package_launches(user=user, root_context=course)
         assert_that(results, has_length(1))
+        
+        timestamp_max = timestamp_type(datetime.max)
+        _create_scorm_package_launch_event(user, course, metadata_ntiid, [course_ntiid], timestamp_max)
+        
+        results = get_scorm_package_launches_for_ntiid(metadata_ntiid)
+        assert_that(results, has_length(2))
+        results = get_scorm_package_launches()
+        assert_that(results, has_length(2))
+        assert_that(results[0].timestamp, is_(timestamp_min))
+        assert_that(results[1].timestamp, is_(timestamp_max))
+        results = get_scorm_package_launches(user=user, root_context=course)
+        assert_that(results, has_length(2))
+        results = get_scorm_package_launches(limit=1)
+        assert_that(results, has_length(1))
+        results = get_scorm_package_launches(max_timestamp=datetime.min)
+        assert_that(results, has_length(1))
+        results = get_scorm_package_launches(order_by='timestamp')
+        assert_that(results, has_length(2))
+        assert_that(results[0].timestamp, is_(timestamp_max))
+        assert_that(results[1].timestamp, is_(timestamp_min))     
