e13c234c6d7f103606b9f3ccaee33728b174d649
cutz <christopher.m.utz@gmail.com>
2018-07-05 16:21:54 -0500

    analytics event takes asset instead of metadata

--- src/nti/analytics/database/lti.py
+++ src/nti/analytics/database/lti.py
@@ -26,14 +26,14 @@ from nti.analytics.identifier import get_ntiid_id
 from nti.analytics_database.lti import LTIAssetLaunches
 
 
-def create_launch_record(user, course, metadata, nti_session, context_path, timestamp):
+def create_launch_record(user, course, asset, nti_session, context_path, timestamp):
 
     db = get_analytics_db()
 
     user_record = get_or_create_user(user)
     uid = user_record.user_id
     sid = nti_session
-    rid = get_ntiid_id(metadata)
+    rid = get_ntiid_id(asset)
     rid = get_resource_id(db, rid, create=True)
     timestamp = timestamp_type(timestamp)
     context_path = get_context_path(context_path)
--- src/nti/analytics/lti.py
+++ src/nti/analytics/lti.py
@@ -17,12 +17,12 @@ get_lti_asset_launches = db_lti.get_launch_records
 get_lti_asset_launches_for_ntiid = db_lti.get_launch_records_for_ntiid
 
 
-def _add_launch_record(user, course, metadata, nti_session, timestamp):
+def _add_launch_record(user, course, asset, nti_session, timestamp):
     context_path = [course.ntiid]
-    db_lti.create_launch_record(user, course, metadata, nti_session, context_path, timestamp)
+    db_lti.create_launch_record(user, course, asset, nti_session, context_path, timestamp)
 
 
 @component.adapter(ILTILaunchEvent)
 def _lti_asset_launched(event):
     nti_session = get_nti_session_id()
-    _add_launch_record(event.user, event.course, event.metadata, nti_session, event.timestamp)
+    _add_launch_record(event.user, event.course, event.asset, nti_session, event.timestamp)
