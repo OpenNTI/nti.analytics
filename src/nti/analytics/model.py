bb2faa14610ec594e06669420b896aabb002860f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-09-28 14:10:06 -0500

    pep8; pylint; remove deprecation

--- src/nti/analytics/model.py
+++ src/nti/analytics/model.py
@@ -25,19 +25,19 @@ from nti.analytics.interfaces import IResourceEvent
 from nti.analytics.interfaces import ITopicViewEvent
 from nti.analytics.interfaces import ISurveyViewEvent
 from nti.analytics.interfaces import IAnalyticsSession
-from nti.analytics.interfaces import IAnalyticsSessions
-from nti.analytics.interfaces import IBatchResourceEvents
-from nti.analytics.interfaces import ICourseCatalogViewEvent
-from nti.analytics.interfaces import IUserResearchStatus
-from nti.analytics.interfaces import IUserResearchStatusEvent
-from nti.analytics.interfaces import IAnalyticsClientParams
-from nti.analytics.interfaces import IVideoPlaySpeedChangeEvent
-from nti.analytics.interfaces import ISelfAssessmentViewEvent
-from nti.analytics.interfaces import IAssignmentViewEvent
 from nti.analytics.interfaces import IProfileViewEvent
-from nti.analytics.interfaces import IProfileActivityViewEvent
-from nti.analytics.interfaces import IProfileMembershipViewEvent
+from nti.analytics.interfaces import IAnalyticsSessions
+from nti.analytics.interfaces import IUserResearchStatus
+from nti.analytics.interfaces import IAssignmentViewEvent
+from nti.analytics.interfaces import IBatchResourceEvents
 from nti.analytics.interfaces import IGeographicalLocation
+from nti.analytics.interfaces import IAnalyticsClientParams
+from nti.analytics.interfaces import ICourseCatalogViewEvent
+from nti.analytics.interfaces import ISelfAssessmentViewEvent
+from nti.analytics.interfaces import IUserResearchStatusEvent
+from nti.analytics.interfaces import IProfileActivityViewEvent
+from nti.analytics.interfaces import IVideoPlaySpeedChangeEvent
+from nti.analytics.interfaces import IProfileMembershipViewEvent
 
 from nti.dataserver.interfaces import IUser
 
@@ -48,280 +48,289 @@ from nti.externalization.representation import WithRepr
 
 from nti.property.property import alias
 
-from nti.schema.field import SchemaConfigured
 from nti.schema.fieldproperty import createDirectFieldProperties
+
+from nti.schema.schema import SchemaConfigured
 
 logger = __import__('logging').getLogger(__name__)
 
 
 def _replace_state(obj, old, new):
-	if old in obj.__dict__:
-		value = obj.__dict__.pop(old)
-		if obj.__dict__.get(new, None) is None:
-			obj.__dict__[new] = value
+    if old in obj.__dict__:
+        value = obj.__dict__.pop(old)
+        if obj.__dict__.get(new, None) is None:
+            obj.__dict__[new] = value
 
 
 @WithRepr
 class ViewEvent(SchemaConfigured):
 
-	__external_can_create__ = True
-	time_length = alias('Duration')
-	context_path = None # bwc
+    __external_can_create__ = True
+    time_length = alias('Duration')
+    context_path = None  # bwc
 
-	def __setstate__(self, data):
-		self.__dict__ = data
-		_replace_state(self, 'time_length', 'Duration')
+    def __setstate__(self, data):
+        # pylint: disable=attribute-defined-outside-init
+        self.__dict__ = data
+        _replace_state(self, 'time_length', 'Duration')
 
 
-class RootContextEvent(ViewEvent): # alias
+class RootContextEvent(ViewEvent):  # alias
 
-	course = alias('RootContextID')
+    course = alias('RootContextID')
 
-	def __setstate__(self, data):
-		self.__dict__ = data
-		_replace_state(self, 'time_length', 'Duration')
-		_replace_state(self, 'course', 'RootContextID')
+    def __setstate__(self, data):
+        # pylint: disable=attribute-defined-outside-init
+        self.__dict__ = data
+        _replace_state(self, 'time_length', 'Duration')
+        _replace_state(self, 'course', 'RootContextID')
 
 
 @interface.implementer(IResourceEvent)
 class ResourceEvent(RootContextEvent):
-	createDirectFieldProperties(IResourceEvent)
+    createDirectFieldProperties(IResourceEvent)
 
-	resource_id = alias('ResourceId')
+    resource_id = alias('ResourceId')
 
-	__external_class_name__ = "ResourceEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.resourceevent'
+    __external_class_name__ = "ResourceEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.resourceevent'
 
 
 @interface.implementer(ISelfAssessmentViewEvent)
 class SelfAssessmentViewEvent(RootContextEvent):
-	createDirectFieldProperties(ISelfAssessmentViewEvent)
+    createDirectFieldProperties(ISelfAssessmentViewEvent)
 
-	resource_id = QuestionSetId = alias('ResourceId')
-	content_id = alias('ContentId')
+    resource_id = QuestionSetId = alias('ResourceId')
+    content_id = alias('ContentId')
 
-	__external_class_name__ = "SelfAssessmentViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.selfassessmentviewevent'
+    __external_class_name__ = "SelfAssessmentViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.selfassessmentviewevent'
 
 
 @interface.implementer(IAssignmentViewEvent)
 class AssignmentViewEvent(RootContextEvent):
-	createDirectFieldProperties(IAssignmentViewEvent)
+    createDirectFieldProperties(IAssignmentViewEvent)
 
-	resource_id = AssignmentId = alias('ResourceId')
-	content_id = alias('ContentId')
+    resource_id = AssignmentId = alias('ResourceId')
+    content_id = alias('ContentId')
 
-	__external_class_name__ = "AssignmentViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.assignmentviewevent'
+    __external_class_name__ = "AssignmentViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.assignmentviewevent'
 
 
 @interface.implementer(ISurveyViewEvent)
 class SurveyViewEvent(RootContextEvent):
-	createDirectFieldProperties(ISurveyViewEvent)
+    createDirectFieldProperties(ISurveyViewEvent)
 
-	resource_id = SurveyId = alias('ResourceId')
-	content_id = alias('ContentId')
+    resource_id = SurveyId = alias('ResourceId')
+    content_id = alias('ContentId')
 
-	__external_class_name__ = "SurveyViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.surveyviewevent'
+    __external_class_name__ = "SurveyViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.surveyviewevent'
 
 
 @interface.implementer(IVideoEvent)
 class WatchVideoEvent(RootContextEvent):
-	createDirectFieldProperties(IVideoEvent)
-	PlaySpeed = None # bwc
+    createDirectFieldProperties(IVideoEvent)
+    PlaySpeed = None  # bwc
 
-	__external_class_name__ = "WatchVideoEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.watchvideoevent'
-	event_type = VIDEO_WATCH
+    __external_class_name__ = "WatchVideoEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.watchvideoevent'
+    event_type = VIDEO_WATCH
 
-	course = alias('RootContextID')
-	resource_id = alias('ResourceId')
+    course = alias('RootContextID')
+    resource_id = alias('ResourceId')
 
 
 @interface.implementer(IVideoEvent)
 class SkipVideoEvent(RootContextEvent):
-	createDirectFieldProperties(IVideoEvent)
-	PlaySpeed = None # bwc
+    createDirectFieldProperties(IVideoEvent)
+    PlaySpeed = None  # bwc
 
-	__external_class_name__ = "SkipVideoEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.skipvideoevent'
-	event_type = VIDEO_SKIP
+    __external_class_name__ = "SkipVideoEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.skipvideoevent'
+    event_type = VIDEO_SKIP
 
-	course = alias('RootContextID')
-	resource_id = alias('ResourceId')
+    course = alias('RootContextID')
+    resource_id = alias('ResourceId')
 
 
 @interface.implementer(ICourseCatalogViewEvent)
 class CourseCatalogViewEvent(RootContextEvent):
-	createDirectFieldProperties(ICourseCatalogViewEvent)
+    createDirectFieldProperties(ICourseCatalogViewEvent)
 
-	__external_class_name__ = "CourseCatalogViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.coursecatalogviewevent'
+    __external_class_name__ = "CourseCatalogViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.coursecatalogviewevent'
 
 
 @interface.implementer(IBlogViewEvent)
 class BlogViewEvent(ViewEvent):
-	createDirectFieldProperties(IBlogViewEvent)
+    createDirectFieldProperties(IBlogViewEvent)
 
-	__external_class_name__ = "BlogViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.blogviewevent'
+    __external_class_name__ = "BlogViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.blogviewevent'
+
 
 @interface.implementer(INoteViewEvent)
 class NoteViewEvent(RootContextEvent):
-	createDirectFieldProperties(INoteViewEvent)
+    createDirectFieldProperties(INoteViewEvent)
 
-	__external_class_name__ = "NoteViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.noteviewevent'
+    __external_class_name__ = "NoteViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.noteviewevent'
 
 
 @interface.implementer(ITopicViewEvent)
 class TopicViewEvent(RootContextEvent):
-	createDirectFieldProperties(ITopicViewEvent)
+    createDirectFieldProperties(ITopicViewEvent)
 
-	__external_class_name__ = "TopicViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.topicviewevent'
+    __external_class_name__ = "TopicViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.topicviewevent'
 
 
 @interface.implementer(IVideoPlaySpeedChangeEvent)
 class VideoPlaySpeedChangeEvent(SchemaConfigured):
-	createDirectFieldProperties(IVideoPlaySpeedChangeEvent)
+    createDirectFieldProperties(IVideoPlaySpeedChangeEvent)
 
-	__external_can_create__ = True
-	__external_class_name__ = "VideoPlaySpeedChangeEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.videoplayspeedchange'
+    __external_can_create__ = True
+    __external_class_name__ = "VideoPlaySpeedChangeEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.videoplayspeedchange'
 
-	resource_id = alias('ResourceId')
+    resource_id = alias('ResourceId')
 
-	def __init__(self, *args, **kwargs):
-		SchemaConfigured.__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        SchemaConfigured.__init__(self, *args, **kwargs)
 
 
 @interface.implementer(IProfileViewEvent)
 class ProfileViewEvent(ViewEvent):
-	createDirectFieldProperties(IProfileViewEvent)
+    createDirectFieldProperties(IProfileViewEvent)
 
-	__external_class_name__ = "ProfileViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.profileviewevent'
+    __external_class_name__ = "ProfileViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.profileviewevent'
 
 
 @interface.implementer(IProfileActivityViewEvent)
 class ProfileActivityViewEvent(ViewEvent):
-	createDirectFieldProperties(IProfileActivityViewEvent)
+    createDirectFieldProperties(IProfileActivityViewEvent)
 
-	__external_class_name__ = "ProfileActivityViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.profileactivityviewevent'
+    __external_class_name__ = "ProfileActivityViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.profileactivityviewevent'
 
 
 @interface.implementer(IProfileMembershipViewEvent)
 class ProfileMembershipViewEvent(ViewEvent):
-	createDirectFieldProperties(IProfileMembershipViewEvent)
+    createDirectFieldProperties(IProfileMembershipViewEvent)
 
-	__external_class_name__ = "ProfileMembershipViewEvent"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.profilemembershipviewevent'
+    __external_class_name__ = "ProfileMembershipViewEvent"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.profilemembershipviewevent'
 
 
 @interface.implementer(IBatchResourceEvents)
 @WithRepr
 @NoPickle
 class BatchResourceEvents(SchemaConfigured):
-	createDirectFieldProperties(IBatchResourceEvents)
+    createDirectFieldProperties(IBatchResourceEvents)
 
-	__external_can_create__ = True
-	__external_class_name__ = "BatchResourceEvents"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.batchevents'
+    __external_can_create__ = True
+    __external_class_name__ = "BatchResourceEvents"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.batchevents'
 
-	def __iter__(self):
-		return iter( self.events )
+    def __iter__(self):
+        # pylint: disable=no-member
+        return iter(self.events)
 
-	def __len__(self):
-		return len( self.events )
+    def __len__(self):
+        # pylint: disable=no-member
+        return len(self.events)
 
 
 @interface.implementer(IAnalyticsSessions)
 @WithRepr
 @NoPickle
 class AnalyticsSessions(SchemaConfigured):
-	createDirectFieldProperties(IAnalyticsSessions)
+    createDirectFieldProperties(IAnalyticsSessions)
 
-	__external_can_create__ = True
-	__external_class_name__ = "AnalyticsSessions"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.analyticssessions'
+    __external_can_create__ = True
+    __external_class_name__ = "AnalyticsSessions"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.analyticssessions'
 
-	def __iter__(self):
-		return iter( self.sessions )
+    def __iter__(self):
+        # pylint: disable=no-member
+        return iter(self.sessions)
 
-	def __len__(self):
-		return len( self.sessions )
+    def __len__(self):
+        # pylint: disable=no-member
+        return len(self.sessions)
 
 
 @interface.implementer(IAnalyticsSession)
 @WithRepr
 class AnalyticsSession(SchemaConfigured):
-	createDirectFieldProperties(IAnalyticsSession)
+    createDirectFieldProperties(IAnalyticsSession)
 
-	__external_can_create__ = True
-	__external_class_name__ = "AnalyticsSession"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.analyticssession'
+    __external_can_create__ = True
+    __external_class_name__ = "AnalyticsSession"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.analyticssession'
 
-	def __init__(self, *args, **kwargs):
-		SchemaConfigured.__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        SchemaConfigured.__init__(self, *args, **kwargs)
 
 
 @interface.implementer(IGeographicalLocation)
 @WithRepr
 class GeographicalLocation(SchemaConfigured):
-	createDirectFieldProperties(IGeographicalLocation)
+    createDirectFieldProperties(IGeographicalLocation)
 
-	__external_class_name__ = "GeographicalLocation"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.geographicallocation'
+    __external_class_name__ = "GeographicalLocation"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.geographicallocation'
 
-	def __init__(self, *args, **kwargs):
-		SchemaConfigured.__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        SchemaConfigured.__init__(self, *args, **kwargs)
 
 
 @interface.implementer(IUserResearchStatusEvent)
 class UserResearchStatusEvent(ObjectEvent):
 
-	def __init__(self, user, allow_research):
-		super(UserResearchStatusEvent, self).__init__(user)
-		self.allow_research = allow_research
+    def __init__(self, user, allow_research):
+        super(UserResearchStatusEvent, self).__init__(user)
+        self.allow_research = allow_research
 
-	@property
-	def user(self):
-		return self.object
+    @property
+    def user(self):
+        return self.object
+
 
 @component.adapter(IUser)
 @interface.implementer(IUserResearchStatus)
 class _Researchable(PersistentCreatedAndModifiedTimeObject):
 
-	_SET_CREATED_MODTIME_ON_INIT = False
+    _SET_CREATED_MODTIME_ON_INIT = False
 
-	def __init__(self):
-		PersistentCreatedAndModifiedTimeObject.__init__(self)
-		self.allow_research = False
-		self.lastModified = None
+    def __init__(self):
+        PersistentCreatedAndModifiedTimeObject.__init__(self)
+        self.allow_research = False
+        self.lastModified = None
 
 
 _UserResearchStatus = an_factory(_Researchable, 'research_status')
 
 
 def delete_research_status(user):
-	try:
-		annotations = user.__annotations__
-		annotations.pop('research_status', None)
-	except AttributeError:
-		pass
+    try:
+        annotations = user.__annotations__
+        annotations.pop('research_status', None)
+    except AttributeError:
+        pass
 
 
 @interface.implementer(IAnalyticsClientParams)
 @WithRepr
 class AnalyticsClientParams(SchemaConfigured):
-	createDirectFieldProperties(IAnalyticsClientParams)
+    createDirectFieldProperties(IAnalyticsClientParams)
 
-	__external_can_create__ = True
-	__external_class_name__ = "AnalyticsClientParams"
-	mime_type = mimeType = 'application/vnd.nextthought.analytics.analyticsclientparams'
+    __external_can_create__ = True
+    __external_class_name__ = "AnalyticsClientParams"
+    mime_type = mimeType = 'application/vnd.nextthought.analytics.analyticsclientparams'
 
-	def __init__(self, *args, **kwargs):
-		SchemaConfigured.__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        SchemaConfigured.__init__(self, *args, **kwargs)
